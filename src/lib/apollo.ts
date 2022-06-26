cabdc9285280db1eb9f69cff408e3223ee2f5d7f
unknown <dayna.prado@lecom.com.br>
2022-06-26 18:35:57 -0300

    Aula 04 - Criando o Subscribe

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4psknbo290m01xthmfu6oz2/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
