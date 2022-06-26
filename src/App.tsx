c2655b68ed6444e9734fffff024798c600d4b359
unknown <dayna.prado@lecom.com.br>
2022-06-26 16:00:28 -0300

    Aula 03 - Criando área de vídeo e roteamento de Lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { ApolloProvider } from "@apollo/client"
 import { Event } from "./pages/Event"
+import { client } from './lib/apollo'
+import { Router } from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   )
 }
